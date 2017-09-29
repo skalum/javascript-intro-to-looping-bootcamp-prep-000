246781bd542d7dd0f120696b4c346528235213cb
skalum <stkalum@gmail.com>
2017-09-29 19:07:55 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,3 +1,7 @@
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i == 1) {
@@ -6,6 +10,22 @@ function forLoop(array) {
       array.push("I am ${i} strange loops.")
     }
   }
+
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+
+  return "done";
+}
+
+function doWhileLoop(array) {
+  do {
+    array.shift();
+  } while (array.length > 0 && maybeTrue());
 
   return array;
 }
